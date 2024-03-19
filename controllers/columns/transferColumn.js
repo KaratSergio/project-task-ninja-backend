54d0f63409b58b577c6cc1f6f42bd06e514aca3a
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-19 22:28:04 +0200

    add controller transferColumn

--- controllers/columns/transferColumn.js
+++ controllers/columns/transferColumn.js
@@ -7,7 +7,23 @@ import { transferSchema } from "../../schemas/schemaTransfer.js";
 import { BadRequestError } from "../../helpers/BadRequestError.js";
 
 const transferColumn = async (req, res) => {
-  //
+  const { value, error } = transferSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (error) BadRequestError(error);
+  const { id } = req.params;
+  const columnId = new Types.ObjectId(id);
+  const { source, destination } = value;
+  await Board.findByIdAndUpdate(source.droppableId, {
+    $pull: { columnOrder: { $in: columnId } },
+  });
+  await Board.findByIdAndUpdate(destination.droppableId, {
+    $push: { columnOrder: { $each: [columnId], $position: destination.index } },
+  });
+  res.json({
+    board: destination.droppableId,
+    message: `Column position has been changed to ${destination.index} on board ${destination.droppableId}`,
+  });
 };
 
 export default transferColumn;
