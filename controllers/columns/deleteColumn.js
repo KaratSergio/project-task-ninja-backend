0062644d697eaf7dc3eaa3e8a3edd6b078d781bb
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-19 22:11:38 +0200

    add controller deleteColumn

--- controllers/columns/deleteColumn.js
+++ controllers/columns/deleteColumn.js
@@ -5,7 +5,21 @@ import { Board } from "../../models/board.js";
 import { Task } from "../../models/task.js";
 
 const deleteColumn = async (req, res) => {
-  //
+  const { id } = req.params;
+  const result = await Column.findByIdAndRemove(id);
+  if (!result) {
+    throw HttpError(404, `Column ${id} not found`);
+  }
+  if (result.taskOrder.length !== 0) {
+    await Task.deleteMany({ _id: { $in: result.taskOrder } });
+  }
+  await Board.findByIdAndUpdate(result.board, {
+    $pull: { columnOrder: result > _id },
+  });
+  res.status(200).json({
+    message: `Column ${id} deleted successfully`,
+    data: result,
+  });
 };
 
 export default deleteColumn;
