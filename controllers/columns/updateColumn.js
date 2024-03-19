585f4ba431d566f98c0967a49d894daa3ad568b7
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-19 22:17:12 +0200

    add controller updateColumn

--- controllers/columns/updateColumn.js
+++ controllers/columns/updateColumn.js
@@ -6,7 +6,17 @@ import { columnSchemas } from "../../schemas/schemaColumn.js";
 import { Column } from "../../models/column.js";
 
 const updateColumn = async (req, res) => {
-  //
+  const { value, error } = columnSchemas.updateColumnSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (error) BadRequestError(error);
+  const { id } = req.params;
+  const { title } = value;
+  const result = await Column.findByIdAndUpdate(id, { title }, { new: true });
+  if (!result) {
+    throw HttpError(404, `Column ${id} not found`);
+  }
+  res.status(200).json(result);
 };
 
 export default updateColumn;
