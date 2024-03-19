71acea1c3d94ca427dc175d072c1167825bdf147
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-19 22:04:58 +0200

    add controller addColumn

--- controllers/columns/addColumn.js
+++ controllers/columns/addColumn.js
@@ -6,7 +6,14 @@ import { Column } from "../../models/column.js";
 import { Board } from "../../models/board.js";
 
 const addColumn = async (req, res) => {
-  //
+  const { value, error } = columnSchemas.addColumnSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (error) BadRequestError(error);
+  const { board } = value;
+  const result = await Column.create({ ...value });
+  await Board.findByIdAndUpdate(board, { $push: { columnOrder: result._id } });
+  res.status(201).json(result);
 };
 
 export default addColumn;
