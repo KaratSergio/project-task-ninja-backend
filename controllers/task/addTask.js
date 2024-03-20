910452b0cf892bc51d3a2f5243a65618a0954e0d
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-20 20:37:34 +0200

    addTask+deleteTask controllers

--- controllers/task/addTask.js
+++ controllers/task/addTask.js
@@ -9,6 +9,15 @@ const addTask = async (req, res) => {
   const { value, error } = taskSchemas.addTaskSchema.validate(req.body, {
     abortEarly: false,
   });
+  if (error) BadRequestError(error);
+
+  const { column } = value;
+  const result = await Task.create({ ...value });
+
+  const { board } = await Column.findByIdAndUpdate(column, {
+    $push: { taskOrder: result._id },
+  });
+  res.status(201).json({ ...result._doc, board });
 };
 
 export default addTask;
--- controllers/task/deleteTask.js
+++ controllers/task/deleteTask.js
@@ -4,7 +4,20 @@ import { Column } from "../../models/column.js";
 import { HttpError } from "../../helpers/HttpError.js";
 
 const deleteTask = async (req, res) => {
-  //
+  const { id } = req.params;
+  const result = await Task.findByIdAndRemove(id);
+  if (!result) {
+    throw HttpError(404, `Task ${id} not found`);
+  }
+
+  const { board } = await Column.findByIdAndUpdate(result.column, {
+    $pull: { taskOrder: result._id },
+  });
+  result.board = board;
+  res.status(200).json({
+    message: `Task ${id} deleted successfully`,
+    data: { ...result._doc, board },
+  });
 };
 
 export default deleteTask;
