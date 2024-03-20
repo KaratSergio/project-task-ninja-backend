fb1315ad45ce2d9935db83a08ea0639947d849a4
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-20 20:52:54 +0200

    Update updateTask.js

--- controllers/task/updateTask.js
+++ controllers/task/updateTask.js
@@ -7,7 +7,19 @@ import { HttpError } from "../../helpers/HttpError.js";
 import { BadRequestError } from "../../helpers/BadRequestError.js";
 
 const updateTask = async (req, res) => {
-  //
+  const { value, error } = taskSchemas.updateTaskSchemas.validate(req.body, {
+    abortEarly: false,
+  });
+  if (error) BadRequestError(error);
+
+  const { id } = req.params;
+  const result = await Task.findByIdAndUpdate(id, value, { new: true });
+  if (!result) {
+    throw HttpError(404, `Task ${id} not found`);
+  }
+
+  const { board } = await Column.findById(result.column);
+  res.json({ ...result._doc, board });
 };
 
 export default updateTask;
