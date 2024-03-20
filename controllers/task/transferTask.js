63e3b469d98397f5de87b64e3585e2e1daaf3166
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-20 20:49:47 +0200

    add transferTask controller

--- controllers/task/transferTask.js
+++ controllers/task/transferTask.js
@@ -8,7 +8,30 @@ import { transferSchema } from "../../schemas/schemaTransfer.js";
 import { BadRequestError } from "../../helpers/BadRequestError.js";
 
 const transferTask = async (req, res) => {
-  //
+  const { value, error } = transferSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (error) BadRequestError(error);
+
+  const { id } = req.params;
+  const { source, destination } = value;
+
+  const taskId = new mongoose.Types.ObjectId(id);
+  const columnId = new mongoose.Types.ObjectId(destination.droppableId);
+
+  await Column.findByIdAndUpdate(source.droppableId, {
+    $pull: { taskOrder: { $in: taskId } },
+  });
+
+  const { board } = await Column.findByIdAndUpdate(destination.droppableId, {
+    $push: { taskOrder: { $each: [taskId], $position: destination.index } },
+  });
+
+  await Task.findByIdAndUpdate(id, { column: columnId });
+  res.json({
+    board,
+    message: `Task position has been changed to ${destination.index} in column ${destination.droppableId}`,
+  });
 };
 
-export default transferSchema;
+export default transferTask;
