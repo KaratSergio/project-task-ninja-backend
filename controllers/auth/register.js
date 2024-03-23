4000314f3b7c625ffcbd81492463006360a11d07
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-23 16:40:23 +0200

    auth register controller

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -7,7 +7,19 @@ import authSchema from "../../schemas/schemaAuth.js";
 import BadRequestError from "../../helpers/BadRequestError.js";
 
 const register = async (req, res) => {
-  //
+  const { value, error } = authSchema.registerSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  const { name, email, password } = value;
+  if (error) BadRequestError(error);
+
+  const userEmail = await User.findOne({ email });
+  if (userEmail) {
+    throw HttpError(409, "Email has already in use");
+  }
+  const result = await User.create({ name, email, password });
+
+  res.status(201).json(result);
 };
 
 export default register;
