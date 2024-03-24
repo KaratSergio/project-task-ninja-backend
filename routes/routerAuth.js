b8018bee026c839c7c8d207f87d344e31800ac78
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-24 12:41:17 +0200

    add routes Auth\Boards

--- routes/routerAuth.js
+++ routes/routerAuth.js
@@ -1,1 +1,36 @@
 import express from "express";
+
+import help from "../controllers/help/help.js";
+import login from "../controllers/auth/login.js";
+import logout from "../controllers/auth/logout.js";
+import register from "../controllers/auth/register.js";
+import updateUser from "../controllers/auth/updateUser.js";
+import currentUser from "../controllers/auth/currentUser.js";
+import changeTheme from "../controllers/auth/changeTheme.js";
+import googleAuth from "../controllers/auth/googleAuth.js";
+import googleRedirect from "../controllers/auth/googleRedirect.js";
+
+import upload from "../middlewares/upload.js";
+import isAuthorized from "../middlewares/isAuthorized.js";
+
+const routerAuth = express.Router();
+
+routerAuth.get("/google", googleAuth);
+
+routerAuth.get("/google-redirect", googleRedirect);
+
+routerAuth.get("/current", isAuthorized, currentUser);
+
+routerAuth.post("/register", register);
+
+routerAuth.post("/login", login);
+
+routerAuth.post("/logout", isAuthorized, logout);
+
+routerAuth.post("/help", isAuthorized, help);
+
+routerAuth.patch("/theme", isAuthorized, changeTheme);
+
+routerAuth.put("/update", isAuthorized, upload.single("avatar"), updateUser);
+
+export default routerAuth;
