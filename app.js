35ef9b4477dfa56905e64dcc5203079a5149aa72
Sergio <137483700+KaratSergio@users.noreply.github.com>
2024-03-24 15:10:17 +0200

    add routes

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
 
+import routerAuth from "./routes/routerAuth.js";
+import routerTasks from "./routes/routerTasks.js";
+import routerBoards from "./routes/routerBoards.js";
+import routerColumns from "./routes/routerColumns.js";
+
 export const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
+
+app.use("/api/users", routerAuth);
+app.use("/api/tasks", routerTasks);
+app.use("/api/boards", routerBoards);
+app.use("/api/columns", routerColumns);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not Found" });
@@ -17,3 +28,5 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server Error" } = err;
   res.status(status).json({ message });
 });
+
+export default app;
--- server.js
+++ server.js
@@ -2,15 +2,16 @@ import mongoose from "mongoose";
 
 import { app } from "./app.js";
 
-const { DB_HOST, PORT } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
-    console.log("Server started at port " + PORT);
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
   })
   .catch((error) => {
     console.log(error.message);
